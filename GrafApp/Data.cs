3c13d114ae3449366078718946f60f9d4ba72356
bratello-0 <bro4bet69K@yandex.ru>
2023-03-12 19:22:52 +0400

    added save and load functions

--- GrafApp/Data.cs
+++ GrafApp/Data.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace GrafApp
 {
-    internal class Data
+    [Serializable]
+    public class Data
     {
+        [XmlElement("Matrix")]
+        public List<string> Matrix { get; set; }
+
+        [XmlElement("Result")]
+        public string Result { get; set; }
+
+        public Data() {
+        }
+        public Data(List<string> matrix) {
+            Matrix = matrix;
+        }
     }
 }
