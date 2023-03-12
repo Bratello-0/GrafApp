0433775051909034d71f32b9b28b83879addd6ab
bratello-0 <bro4bet69K@yandex.ru>
2023-03-12 19:12:50 +0400

    added save and load functions

--- GrafApp/DataController.cs
+++ GrafApp/DataController.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace GrafApp
 {
-    internal class DataController
+    public static class DataController
     {
+
+        public static bool IsFile(string path) => File.Exists(path);
+
+        public static Data Load(string path)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Data));
+            Data data = null;
+
+            if(!IsFile(path)) throw new Exception();
+
+            using (FileStream file = new FileStream(path, FileMode.Open))
+            {
+                data = xmlSerializer.Deserialize(file) as Data;
+            }
+            return data;
+        }
+
+        public static void Save(Data data, string path)
+        {
+            if (data == null) throw new Exception();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Data));
+            using (FileStream file = new FileStream(path, FileMode.Create))
+            {
+                xmlSerializer.Serialize(file, data);
+            }
+        }
     }
 }
